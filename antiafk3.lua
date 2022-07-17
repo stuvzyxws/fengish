2d3e5c807334d56904238f18bd9b25646ea37485
stuvzyxws <102387394+stuvzyxws@users.noreply.github.com>
2022-07-16 23:38:02 -0400

    Add files via upload

---
+++ antiafk3.lua
@@ -0,0 +1,7 @@
+wait(30)
+print("Anti afk loaded")
+local VirtualUser=game:service'VirtualUser'
+game:service'Players'.LocalPlayer.Idled:connect(function()
+VirtualUser:CaptureController()
+VirtualUser:ClickButton2(Vector2.new())
+end)
